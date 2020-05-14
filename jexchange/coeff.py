f10736e3915652215680d759f8f6727f6de35a66
Wei Chen <waynechen@gmail.com>
2020-05-14 20:18:15 +0200

    debug

--- jexchange/coeff.py
+++ jexchange/coeff.py
@@ -40,7 +40,6 @@ class HSCoeff:
             for idx, site_p in enumerate(self.s.sites):
                 nn_sites = [nn_info[2] for nn_info in \
                     self.s.get_neighbors_in_shell(site_p.coords,r,0.2,include_index=True)]
-                for n in nn_sites:
-                    cj[jn] += self.spin[idx]*self.spin[n] 
+                cj[jn] += sum(self.spin[idx]*self.spin[n] for n in nn_sites)
             cj[jn] /= self.s.num_sites
         return cj
